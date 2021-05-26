fe8094b205f3b708424c66fedb258dddd494a7fe
Administrator <120769563@qq.com>
2021-05-26 09:10:15 +0800

    2021.05.26 09:09:52 整合spring和hibernate 2

--- src/net/imwork/zhanlong/spring_hibernate/client/Test.java
+++ src/net/imwork/zhanlong/spring_hibernate/client/Test.java
@@ -5,8 +5,6 @@ import net.imwork.zhanlong.spring_hibernate.service.PersonService;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.List;
-
 /**
  * @author Administrator
  */
@@ -18,20 +16,21 @@ public class Test
         BeanFactory factory = new ClassPathXmlApplicationContext("spring_hibernate-config.xml");
 
         PersonService service = (PersonService)factory.getBean("personService");
+/*
+        Person p = service.getPersonById(1);
+        System.out.println(p);
+*/
+        Person person = new Person();
+        person.setName("zhanlong");
+        person.setAge(26);
+        service.save(person);
+/**/
+//        service.delete(1);
 
-//        Person p = service.getPersonById(1);
-//        System.out.println(p);
-//
-//        Person person = new Person();
-//        person.setName("zhanlong");
-//        person.setAge(26);
-//        service.save(person);
-
-////        service.delete(1);
-//
+        /*
         List<Person> list = service.getPersonsByName("zhanlong");
         System.out.println(list.get(0).getName());
-
+*/
     }
 
 }
